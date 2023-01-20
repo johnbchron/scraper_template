fe8fc69437ca8ea6f3de0d4f7242dc2374852ba2
John Lewis <johnbchron@outlook.com>
2023-01-20 16:40:17 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 PROJECT_NAME=$(basename `pwd`)
+
+mkdir -p data
+
 docker build . -t "$PROJECT_NAME" &&
 
 echo "
@@ -10,4 +13,5 @@ docker run \
   --rm \
   --name "$PROJECT_NAME" \
   --mount type=bind,source="$(pwd)"/data,target=/app/data \
+  --network=host \
   "$PROJECT_NAME"
